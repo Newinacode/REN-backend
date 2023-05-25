fd80c27bd46a77ac2a36bb20acbac4dba22e16d6
pawanphuyal23 <66936803+pawanphuyal23@users.noreply.github.com>
2023-05-25 21:56:14 +0545

    commit

--- project/settings.py
+++ project/settings.py
@@ -218,3 +218,4 @@ EMAIL_USE_TLS = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
